d40dd3d58fed5bbcb40c755fb237b45d73ec0f3a
ZoeBetta <77151364+ZoeBetta@users.noreply.github.com>
2022-09-06 06:05:10 -0400

    start debug

--- zoe/scripts/stairs.py
+++ zoe/scripts/stairs.py
@@ -36,12 +36,12 @@ from sensor_msgs.msg import PointCloud2
 from sensor_msgs.msg import PointField
 from std_msgs.msg import Header
 from zoe.msg import fiducial
+from nav_msgs.msg import Odometry
 
 from tf import transformations
-from rrt_exploration.srv import Floor, FloorRequest
 
 LOGGER = logging.getLogger(__name__)
-
+odom=Odometry()
 def _update_thread(async_task):
     while True:
         async_task.update()
@@ -57,12 +57,17 @@ class AsyncRobotState(AsyncPeriodicQuery):
     def _start_query(self):
         return self._client.get_robot_state_async()
 
+def clbkodom(msg):
+    odom=msg
+    #print(odom)
+
+
 def main(argv):
     # The last argument should be the IP address of the robot. The app will use the directory to find
     # the velodyne and start getting data from it.
-    rospy.init_node('fiducialdetection')
+    rospy.init_node('stairs_detection')
 
-    sdk = bosdyn.client.create_standard_sdk('FiducialClient')
+    sdk = bosdyn.client.create_standard_sdk('Stairs')
     robot = sdk.create_robot('192.168.80.3')
     robot.authenticate('user', 'wruzvkg4rce4')
     #bosdyn.client.util.authenticate(robot)
@@ -71,7 +76,7 @@ def main(argv):
    # _point_cloud_client = robot.ensure_client('velodyne-point-cloud')
     _robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
     _robot_state_task = AsyncRobotState(_robot_state_client)
-    _world_object_client = robot.ensure_client(WorldObjectClient.default_service_name)
+    #_world_object_client = robot.ensure_client(WorldObjectClient.default_service_name)
     _task_list = [_robot_state_task]
     _async_tasks = AsyncTasks(_task_list)
 
@@ -82,59 +87,35 @@ def main(argv):
     print('Connected.')
     # initialization of the publisher
     pub = rospy.Publisher('/fiducials', fiducial, queue_size=10)
+    rospy.Subscriber('spot/odometry', Odometry, clbkodom)
     msg=fiducial()
     # initialization of tf
     listener = tf.TransformListener()
-   
+    body_tform_odom=[]
     #update_thread = threading.Thread(target=_update_thread, args=[_async_tasks])
     #update_thread.daemon = True
     #update_thread.start()
-    floor_service=rospy.ServiceProxy('retrievefloor', Floor)
-    rospy.wait_for_service('retrievefloor')
+   
     # Wait for the first responses.
     while not rospy.is_shutdown():
-        request_fiducials = [world_object_pb2.WORLD_OBJECT_APRILTAG]
-        fiducial_objects = _world_object_client.list_world_objects(object_type=request_fiducials).world_objects
+        #request_fiducials = [world_object_pb2.WORLD_OBJECT_APRILTAG]
+        #fiducial_objects = _world_object_client.list_world_objects(object_type=request_fiducials).world_objects
         try:
-            (trans,rot) = listener.lookupTransform('/start', BODY_FRAME_NAME, rospy.Time(0))
+            (trans,rot) = listener.lookupTransform('/start', '/odom', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-        rospy.wait_for_service('retrievefloor')
-        req=FloorRequest()
-        req.req=0
-        fl=floor_service(req)
-        floor=fl.floor
         #print(trans)
         #print(rot)
-        for i in fiducial_objects:
-            #print(fiducial_objects)
-            #print(fiducial_objects.name)
-            #print(i.apriltag_properties.frame_name_fiducial)
-            # it sends the message with the name of the fiducial and its position
-            name=i.apriltag_properties.frame_name_fiducial
-            id_str=name.split("_")
-            id_int=int(id_str[1])
-            msg.id=id_int
-            #retrieve position in frame start
-            vision_tform_fiducial = get_a_tform_b(i.transforms_snapshot, BODY_FRAME_NAME, i.apriltag_properties.frame_name_fiducial).to_proto()
-            euler=transformations.euler_from_quaternion(rot)
-            trMat=np.array([ [np.cos(euler[0])*np.cos(euler[1])*np.cos(euler[2])-np.sin(euler[0])*np.sin(euler[2]), -np.cos(euler[0])*np.cos(euler[1])*np.sin(euler[2])-np.sin(euler[0])*np.cos(euler[2]), np.cos(euler[0])*np.sin(euler[1]), trans[0]],
+        euler=transformations.euler_from_quaternion(rot)
+        trMat=np.array([ [np.cos(euler[0])*np.cos(euler[1])*np.cos(euler[2])-np.sin(euler[0])*np.sin(euler[2]), -np.cos(euler[0])*np.cos(euler[1])*np.sin(euler[2])-np.sin(euler[0])*np.cos(euler[2]), np.cos(euler[0])*np.sin(euler[1]), trans[0]],
             [np.sin(euler[0])*np.cos(euler[1])*np.cos(euler[2])-np.cos(euler[0])*np.sin(euler[2]), -np.sin(euler[0])*np.cos(euler[1])*np.sin(euler[2])+np.cos(euler[0])*np.cos(euler[2]), np.sin(euler[0])*np.sin(euler[1]), trans[1]],
             [-np.sin(euler[1])*np.cos(euler[2]), np.sin(euler[1])*np.sin(euler[2]), np.cos(euler[1]), trans[2]],
             [0,0,0,1]
             ]
             )
-            body_tform_fid=[vision_tform_fiducial.position.x, vision_tform_fiducial.position.y, vision_tform_fiducial.position.z, 1]
-            start_tform_fiducial=np.matmul(trMat,body_tform_fid)
-            #print(start_tform_fiducial)
-            #print('transform')
-            #print(vision_tform_fiducial.position)
-            # do transformation matrix between vision and transform
-            msg.x=start_tform_fiducial[0]
-            msg.y=start_tform_fiducial[1]
-            msg.z=start_tform_fiducial[2]
-            msg.floor=floor
-            pub.publish(msg)
+        body_tform_odom=[odom.pose.pose.position.x,odom.pose.pose.position.y,odom.pose.pose.position.z,1]
+        pos_tform_start= np.matmul(trMat,body_tform_odom)
+        print(pos_tform_start)
 
 
 if __name__ == '__main__':
